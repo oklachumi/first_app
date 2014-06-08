485fe938a96a7d52f7ed01f3069a5144ac1c2a45
oklachumi <alphaarbutin@gmail.com>
2014-06-09 00:12:04 +0800

    userindices_unique_true

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
+		before_save ( self.email = email.downcase )
 	has_secure_password
 	validates :name, presence: true, length: { maximum: 30 }
 	validates :email, format: { with: /\A[^@]+@[^@]+\z/ }, uniqueness: true
+#	validates :email, format: { with: /\A[^@]+@[^@]+\z/ }, uniqueness: ( case_sensitive: false )
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606162453) do
+ActiveRecord::Schema.define(version: 20140608160527) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20140606162453) do
     t.datetime "updated_at"
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
