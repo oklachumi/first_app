24618a992f1ac9a26a3f8c34f6db4b01f7d13e8d
oklachumi <alphaarbutin@gmail.com>
2014-06-11 01:52:04 +0800

    gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-		before_save ( self.email = email.downcase )
+		#before_save ( self.email = email.downcase )
 	has_secure_password
 	validates :name, presence: true, length: { maximum: 30 }
 	validates :email, format: { with: /\A[^@]+@[^@]+\z/ }, uniqueness: true
